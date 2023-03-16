8116a68ed4d96bb1fc23cb564895756e3f33c605
hyejinbaek <bhj4208@gmail.com>
2023-03-16 21:03:25 +0900

    code analysis standardscaler()

--- dynamic_imputation/test/preprocessing.py
+++ dynamic_imputation/test/preprocessing.py
@@ -11,7 +11,9 @@ def missing_value_generator(X, missing_rate, seed):
     missing_value_average_each_row = column_num * (missing_rate/100)
 
     np.random.seed(seed)
+    # rvs 함수 : 무작위로 표본을 만들때 생성
     poisson_dist = poisson.rvs(mu = missing_value_average_each_row, size = row_num, random_state = seed)
+    # numpy.clip(array, min, max) : array 내의 element들에 대해서 min 값보다 작은값들을 min으로 바꿔주고 max값보다 큰 값들을 max값으로 바꿔주는 함수
     poisson_dist = np.clip(poisson_dist, 0, X.shape[1]-1)
     
     column_idx = np.arange(column_num)
@@ -29,6 +31,8 @@ def preprocessing(x_trnval, x_tst, y_trnval, y_tst, missing_rate, seed):
     
     x_trnval = missing_value_generator(x_trnval, missing_rate, seed)
 
+    # StandardScaler():각 열의 feature 값의 평균을 0으로 잡고, 표준편차를 1로 간주하여 정규화시키는 방법
+    # 각 데이터가 평균에서 몇 표준편차만큼 떨어져있는지를 기준으로 삼고, 데이터의 특징을 모르는 경우 사용하는 정규화 방법
     scaler_x = StandardScaler()
     x_trnval = scaler_x.fit_transform(x_trnval)
     x_tst = scaler_x.transform(x_tst)
