a67f5581123a867022d5039cba3967587c0221d1
hyejinbaek <bhj4208@gmail.com>
2023-05-25 14:13:34 +0900

    modify folder and file

---
+++ 1_single_imputation/breast-cancer dataset/deletion_imputation.py
@@ -0,0 +1,68 @@
+# tensorflow version : 2.9.1
+# deletion imputation
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+from sklearn.model_selection import train_test_split, cross_validate, KFold
+from typing import Tuple
+
+
+data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+df_data = pd.read_csv(data_url)
+col_data = df_data.columns = ['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion ', 'Single Epithelial Cell Size',
+                            'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+train_col = ['Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion ',
+             'Single Epithelial Cell Size','Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses']
+df_data['Bare Nuclei'] = df_data['Bare Nuclei'].replace('?',0).astype(int)
+df_data['Class'] = df_data['Class'].replace({2:0, 4:1})
+
+def create_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(units=32, activation='tanh', input_shape=(X.shape[1],)))
+    model.add(tf.keras.layers.Dense(units=16, activation='tanh'))
+    model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+# define train code
+def cross_valid(
+    X: np.array, y: np.array,
+    cv=KFold(n_splits=5, shuffle=True, random_state=42),
+    scoring=['accuracy', 'f1', 'recall', 'precision'],
+    **kwargs,
+):
+    model_obj = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=create_model, epochs=10, batch_size=32)  # 모델 객체 생성
+    cv_result = cross_validate(model_obj, X, y, cv=cv, scoring=scoring, **kwargs)
+    print("====== cv_result ======", cv_result)
+    for score_name in cv_result:
+        if 'test' in score_name:
+            test_score_mean, test_score_std = np.mean(cv_result[score_name]), np.std(cv_result[score_name])
+            print(f'{score_name}: {test_score_mean:.4f} ± {test_score_std:.4f}')
+
+def set_missing_value(df: pd.DataFrame) -> Tuple[np.array]:
+
+    missing_length = 0.4
+
+    # Create a mask for the NaN values
+    nan_mask = np.random.rand(df.shape[0], df.shape[1]) < missing_length
+
+    # Add the NaN values to the dataframe
+    df[nan_mask] = np.nan
+
+    df = df.copy()
+    print("==== df nan ====", df)
+    df = df.dropna()
+    print("==== df drop ====", df)
+    X = df[train_col].to_numpy()
+    y = df['Class'].to_numpy()
+    # X: features matrix, y: label vector
+    return X, y
+
+if __name__ == '__main__':
+    X, y = set_missing_value(df_data)
+    print("missing data 20% === ")
+    cross_valid(X, y)
---
+++ 1_single_imputation/breast-cancer dataset/hot_deck_imputation.py
@@ -0,0 +1,2 @@
+# tensorflow version : 2.9.1
+# hot-deck imputation
