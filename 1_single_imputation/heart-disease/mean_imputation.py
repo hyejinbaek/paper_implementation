e633249a8346164b6bb8720bc6b9e5fc213a60a1
hyejinbaek <bhj4208@gmail.com>
2023-05-30 20:31:36 +0900

    heart disease dataset code modify

--- 1_single_imputation/heart-disease/mean_imputation.py
+++ 1_single_imputation/heart-disease/mean_imputation.py
@@ -1,14 +1,15 @@
 # tensorflow version : 2.9.1
 # mean imputation
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import train_test_split
 from typing import Tuple
+from sklearn.metrics import accuracy_score
 
 # heart_disease dataset
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
@@ -28,45 +29,30 @@ df_data['thal']= df_data['thal'].astype(dtype='float64')
 print(df_data)
 
 
-def create_model():
+def create_model(input_shape):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(units=32, activation='tanh', input_shape=(X.shape[1],)))
-    #print(" ==== 1 model ====", model)
+    model.add(tf.keras.layers.Dense(units=32, activation='tanh', input_shape=input_shape))
     model.add(tf.keras.layers.Dense(units=16, activation='tanh'))
     model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 # define train code
-def cross_valid(
-    X: np.array, y: np.array,
-    scoring=['accuracy'],
-    **kwargs
-):
-    #model = create_model()
-    model_obj = tf.keras.wrappers.scikit_learn.KerasClassifier(
-        build_fn=create_model, 
-        epochs=10, 
-        batch_size=32
-    )  # 모델 객체 생성
-    #x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    #model_obj.fit(x_train, y_train)  
-    cv_result = cross_validate(model_obj, X, y, cv=5, scoring=scoring, **kwargs)
-    print("====== cv_result ======", cv_result)
-    for score_name in cv_result:
-        if 'test' in score_name:
-            test_score_mean, test_score_std = np.mean(cv_result[score_name]), np.std(cv_result[score_name])
-            print(f'{score_name}: {test_score_mean:.4f} ± {test_score_std:.4f}')
-
-
-
+def cross_valid(X: np.array, y: np.array):
+    acc_list = []
+    for i in range(10):
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=i)
+        model = create_model((X.shape[1],))
+        model.fit(x_train, y_train, epochs=50, batch_size=32, verbose=0)
+        y_pred = (model.predict(x_test) > 0.5).astype(int).flatten()
+        acc = accuracy_score(y_test, y_pred)
+        print(str(i+1)+"th accuracy === : ", acc)
+        acc_list.append(acc)
+    print("mean acc : {}".format(sum(acc_list)/len(acc_list)))
+    print("std acc : {}".format(np.std(acc_list)))
 
 
 def set_missing_value(df: pd.DataFrame) -> Tuple[np.array]:
-    # train_col = [
-    #     "age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach",
-    #     "exang", "oldpeak", "slope", "ca", "thal"
-    # ]
     train_col = [
         "age", "cp", "trestbps", "chol",  "restecg", "thalach",
         "oldpeak", "slope", "ca", "thal"
@@ -74,19 +60,14 @@ def set_missing_value(df: pd.DataFrame) -> Tuple[np.array]:
     missing_length = 0.2
 
     df = df.copy()
-    print("=== original df ===", df)
     for col in train_col:
         nan_mask = np.random.rand(df.shape[0]) < missing_length
         df.loc[nan_mask, col] = np.nan
-    print("===== df nan =====", df)
-    df = df.fillna(df.mean())
-    print("=====df fill na ====", df)
-    X = df[train_col].to_numpy()
-    print("==== X ===", X)
-    y = df['target'].to_numpy()
-    print("==== y ====", y)
-    return X, y
 
+    df = df.fillna(df.mean())
+    X = df[train_col].to_numpy()
+    y = df['target'].to_numpy()
+    return X, y
 
 if __name__ == '__main__':
     X, y = set_missing_value(df_data)
--- 1_single_imputation/heart-disease/mode_imputation.py
+++ 1_single_imputation/heart-disease/mode_imputation.py
@@ -1,20 +1,22 @@
 # tensorflow version : 2.9.1
 # mean imputation
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import train_test_split
 from typing import Tuple
+from sklearn.metrics import accuracy_score
 
 # heart_disease dataset
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
 df_data = pd.read_csv(url, header=None)
 
 #데이터 프레임에 열 이름 추가
+# sex, fbs, exang
 column_names = [
         "age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach",
         "exang", "oldpeak", "slope", "ca", "thal", "target"
@@ -27,55 +29,40 @@ df_data['thal']= df_data['thal'].astype(dtype='float64')
 print(df_data)
 
 
-def create_model():
+def create_model(input_shape):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(units=32, activation='tanh', input_shape=(X.shape[1],)))
-    #print(" ==== 1 model ====", model)
+    model.add(tf.keras.layers.Dense(units=32, activation='tanh', input_shape=input_shape))
     model.add(tf.keras.layers.Dense(units=16, activation='tanh'))
     model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 # define train code
-def cross_valid(
-    X: np.array, y: np.array,
-    scoring=['accuracy'],
-    **kwargs
-):
-    #model = create_model()
-    model_obj = tf.keras.wrappers.scikit_learn.KerasClassifier(
-        build_fn=create_model, 
-        epochs=10, 
-        batch_size=32
-    )  # 모델 객체 생성
-    #x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    #model_obj.fit(x_train, y_train)  
-    cv_result = cross_validate(model_obj, X, y, cv=5, scoring=scoring, **kwargs)
-    print("====== cv_result ======", cv_result)
-    for score_name in cv_result:
-        if 'test' in score_name:
-            test_score_mean, test_score_std = np.mean(cv_result[score_name]), np.std(cv_result[score_name])
-            print(f'{score_name}: {test_score_mean:.4f} ± {test_score_std:.4f}')
-
-
+def cross_valid(X: np.array, y: np.array):
+    acc_list = []
+    for i in range(10):
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=i)
+        model = create_model((X.shape[1],))
+        model.fit(x_train, y_train, epochs=50, batch_size=32, verbose=0)
+        y_pred = (model.predict(x_test) > 0.5).astype(int).flatten()
+        acc = accuracy_score(y_test, y_pred)
+        print(str(i+1)+"th accuracy === : ", acc)
+        acc_list.append(acc)
+    print("mean acc : {}".format(sum(acc_list)/len(acc_list)))
+    print("std acc : {}".format(np.std(acc_list)))
 
 
 def set_missing_value(df: pd.DataFrame) -> Tuple[np.array]:
-    # train_col = [
-    #     "age",'sex', "cp", "trestbps", "chol", "fbs", "restecg", "thalach",
-    #     "exang", "oldpeak", "slope", "ca", "thal"
-    # ]
     train_col = [
         "age", "cp", "trestbps", "chol",  "restecg", "thalach",
         "oldpeak", "slope", "ca", "thal"
     ]
-    missing_length = 0.4
+    missing_length = 0.2
 
     df = df.copy()
     for col in train_col:
         nan_mask = np.random.rand(df.shape[0]) < missing_length
         df.loc[nan_mask, col] = np.nan
-    print("===== df nan =====", df)
     
     # df[train_col].mode().iloc[0]은 각 열의 최빈값을 구한 뒤, 첫 번째 값을 사용하여 결측값을 대체
     df[train_col] = df[train_col].fillna(df[train_col].mode().iloc[0])
