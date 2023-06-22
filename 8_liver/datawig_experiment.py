7adec5f561847182d5b1bc576fd8c6f1dc64c995
hyejinbaek <bhj4208@gmail.com>
2023-06-22 20:26:22 +0900

    change dataset

---
+++ 8_liver/datawig_experiment.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from setproctitle import setproctitle
+import datawig
+import os
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+# CUDA 환경 설정
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+
+# 프로세스 제목 설정
+setproctitle('hyejin')
+
+
+class DynamicImputationModel:
+    def __init__(self, num_layers, num_hidden, dim_y):
+        self.num_layers = num_layers
+        self.num_hidden = num_hidden
+        self.dim_y = dim_y
+        tf.compat.v1.disable_eager_execution()
+        self.x = tf.compat.v1.placeholder(tf.float32, shape=[None, train_X.shape[1]])
+        self.y_true = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
+        self.logits, self.pred = self.build_model(self.x)
+        self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y_true, logits=self.logits))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        self.train_op = self.optimizer.minimize(self.loss)
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+    def build_model(self, x):
+        for _ in range(self.num_layers):
+            x = tf.layers.dense(x, self.num_hidden, activation=tf.nn.tanh)
+        logits = tf.layers.dense(x, self.dim_y)
+
+        if self.dim_y == 1:
+            pred = tf.nn.sigmoid(logits)
+        elif self.dim_y > 2:
+            pred = tf.nn.softmax(logits)
+
+        return logits, pred
+
+    def train_model(self, train_X, train_y, num_epochs, batch_size):
+        num_batches = int(np.ceil(len(train_X) / batch_size))
+        for epoch in range(num_epochs):
+            indices = np.arange(len(train_X))
+            np.random.shuffle(indices)
+            train_X_shuffled = train_X.iloc[indices]
+            train_y_shuffled = train_y.iloc[indices]
+
+            for i in range(num_batches):
+                batch_X = train_X_shuffled.iloc[i * batch_size: (i + 1) * batch_size]
+                batch_y = train_y_shuffled.iloc[i * batch_size: (i + 1) * batch_size]
+
+                self.sess.run(self.train_op, feed_dict={self.x: batch_X.values, self.y_true: batch_y.values.reshape(-1, 1)})
+
+    def get_accuracy(self, x_tst, y_tst):
+        if self.dim_y == 1:
+            pred_Y = tf.cast(self.pred > 0.5, tf.float32)
+            correct_prediction = tf.equal(pred_Y, self.y_true)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+            acc = self.sess.run(accuracy, feed_dict={self.x: x_tst, self.y_true: y_tst})
+
+        else:
+            y_tst_hat = self.sess.run(self.pred, feed_dict={self.x: x_tst})
+            y_tst_hat = np.argmax(y_tst_hat, axis=1)
+
+            acc = accuracy_score(np.argmax(y_tst, axis=1), y_tst_hat)
+
+        return acc
+
+# 데이터 파일 경로 설정
+data_pth = './bupa.data'
+
+# 데이터 불러오기
+df_data = pd.read_csv(data_pth)
+df_data.columns = ['mcv', 'alkphos', 'sgpt', 'sgot', 'gammagt', 'class', 'selector']
+train_col = ['mcv', 'alkphos', 'sgpt', 'sgot', 'gammagt', 'selector']
+data = df_data
+print(" === data 1 type ====", data.dtypes)
+
+missing_length = 0.2
+for col in train_col:
+    nan_mask = np.random.rand(data.shape[0]) < missing_length
+    data.loc[nan_mask, col] = np.nan
+
+data_with_missing = data
+
+# 반복 횟수 설정
+num_iterations = 10
+
+accuracy_list = []
+
+for iteration in range(num_iterations):
+    # Train set과 test set으로 분할
+    train_data, test_data = train_test_split(data_with_missing, test_size=0.2, random_state=iteration)
+
+    # 데이터 결측치 채우기
+    df_train, df_test = datawig.utils.random_split(train_data)
+    print(" === df_train 2 type ====", df_train.dtypes)
+
+
+    imputer = datawig.SimpleImputer(
+        input_columns= train_col,
+        output_column='class',
+        output_path='imputer_model'
+    )
+    imputer.fit(train_df=df_train, num_epochs=50)
+    train_data = imputer.predict(train_data)
+    #print(" ==== imputation train_Data ====", train_data)
+    test_data = imputer.predict(test_data)
+    #print(" ==== imputation test_data ====", test_data)
+
+    # 학습을 위한 데이터 준비
+    train_X = train_data.drop(columns=['class'])
+    train_y = train_data['class']
+    test_X = test_data.drop(columns=['class'])
+    test_y = test_data['class']
+
+    # 신경망 모델 초기화 및 학습
+    model = DynamicImputationModel(num_layers=3, num_hidden=128, dim_y=1)
+    num_epochs = 50
+    batch_size = 32
+
+    model.train_model(train_X, train_y, num_epochs, batch_size)
+    accuracy = model.get_accuracy(test_X.values, test_y.values.reshape(-1, 1))
+    print("==========================================")
+    print(str(iteration+1)+"th accuracy === : ", accuracy)
+    print("==========================================")
+    accuracy_list.append(accuracy)
+    model.sess.close()
+    
+# 평균과 표준편차 계산
+accuracy_mean = np.mean(accuracy_list)
+accuracy_std = np.std(accuracy_list)
+
+print("Mean Accuracy: {:.2f}".format(accuracy_mean))
+print("Standard Deviation of Accuracy: {:.2f}".format(accuracy_std))
+print("==========================================")
+print("=== result : {:.4f} ± {:.4f}".format(sum(accuracy_list)/len(accuracy_list), np.std(accuracy_list)))
+print("==========================================")
+
