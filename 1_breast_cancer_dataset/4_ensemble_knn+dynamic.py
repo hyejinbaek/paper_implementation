a4135148a323ce0f3d69e3c0af4ebf6e8166b3f5
hyejinbaek <bhj4208@gmail.com>
2023-10-25 21:42:35 +0900

    add ensemble knn+dynamic

--- 1_breast_cancer_dataset/4_ensemble_knn+dynamic.py
+++ 1_breast_cancer_dataset/4_ensemble_knn+dynamic.py
@@ -24,7 +24,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.impute import KNNImputer
 
 # CSV 파일 경로 설정
-result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/experiment_result.csv'
+result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/ensemble_method_res.csv'
 
 # 결과를 저장할 리스트 초기화
 results = []
