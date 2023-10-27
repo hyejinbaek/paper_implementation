7c708532b2d99fc0fa3631b5f5232189e415827e
hyejinbaek <bhj4208@gmail.com>
2023-10-27 15:49:10 +0900

    modify result csv path

--- 2_heart_disease/4_ensemble_knn+dynamic.py
+++ 2_heart_disease/4_ensemble_knn+dynamic.py
@@ -22,7 +22,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.impute import KNNImputer
 
 # CSV 파일 경로 설정
-result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/2_heart_ensemble_method_res.csv'
+result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/res/2_heart_ensemble_method_res.csv'
 
 # 결과를 저장할 리스트 초기화
 results = []
