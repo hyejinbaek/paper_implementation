a1374a2fff76ad03cee6f6fccfd615b5cfd70c1e
hyejinbaek <bhj4208@gmail.com>
2023-10-27 15:55:10 +0900

    modify result csv path

--- 7_post_patient/2_ensemble_zero+dynamic.py
+++ 7_post_patient/2_ensemble_zero+dynamic.py
@@ -23,7 +23,7 @@ from math import sqrt
 from sklearn.metrics import accuracy_score
 
 # CSV 파일 경로 설정
-result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/7_post_ensemble_method_res.csv'
+result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/res/7_post_ensemble_method_res.csv'
 
 # 결과를 저장할 리스트 초기화
 results = []
