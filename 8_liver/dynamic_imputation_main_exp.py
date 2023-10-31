bffcec94ea13610c9695d9f3ddc9a566cfcfcdde
hyejinbaek <bhj4208@gmail.com>
2023-10-31 23:14:45 +0900

    command modify

--- 8_liver/dynamic_imputation_main_exp.py
+++ 8_liver/dynamic_imputation_main_exp.py
@@ -1,6 +1,6 @@
 # 데이터셋 변경하여 진행(shuttle-landing-control dataset)
 # tensorflow version : 2.12.0
-# 실행 명령어 : python dynamic_imputation_main_rmse.py --seed 0 --missing_rate 20 --num_mi 5 --m 10 --tau 0.05
+# 실행 명령어 : python dynamic_imputation_main_exp.py --seed 0 --missing_rate 20 --num_mi 5 --m 10 --tau 0.05
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 from setproctitle import *
