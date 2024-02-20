1b5a834fa06c88826f5ec5d39a81b9b2caeb5cfb
hyejinbaek <bhj4208@gmail.com>
2024-02-20 22:34:07 +0900

    change code data preprocessing knn

--- 11_iris/multi_method.py
+++ 11_iris/multi_method.py
@@ -1,6 +1,5 @@
 ## Multi-model ensemble method
 ## nn 제외
-from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
@@ -18,9 +17,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
-from sklearn.decomposition import PCA
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.impute import KNNImputer
 
 # CUDA 환경 설정
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -53,7 +50,7 @@ x = data_with_missing[train_col]
 y = data_with_missing['class']
 
 # 반복 횟수 설정
-num_iterations = 30
+num_iterations = 1
 
 accuracy_list = []
 rmse_list = []  # RMSE 값을 저장할 리스트 추가
@@ -63,41 +60,23 @@ for iteration in range(num_iterations):
     # Train set과 test set으로 분할
     X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=42)
 
-    imputer = SimpleImputer(strategy='most_frequent')
+    imputer = KNNImputer()
     X_train_imputed = imputer.fit_transform(X_train)
-    X_test_imputed = imputer.fit_transform(X_test)
-
-    # Data standardization
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train_imputed)
-    X_test_scaled = scaler.fit_transform(X_test_imputed)
-
-    # PCA 진행
-    pca = PCA()
-    X_train_pca = pca.fit_transform(X_train_scaled)
-    X_test_pca = pca.fit_transform(X_test_scaled)
-
-    # PCA 역변환을 수행하여 원래 feature 공간으로 되돌린다.
-    X_train_imputed_inv = pca.inverse_transform(X_train_pca)
-    X_test_imputed_inv = pca.inverse_transform(X_test_pca)
-    
-    # 표준화된 데이터를 다시 되돌린다.
-    X_train_filled = scaler.inverse_transform(X_train_imputed_inv)
-    X_test_filled = scaler.inverse_transform(X_test_imputed_inv)
+    X_test_imputed = imputer.transform(X_test)
 
     # 2. 모델 앙상블
     # 각각의 분류기를 독립적으로 학습시키고 예측한다고 가정
     xgb_model = XGBClassifier()
-    xgb_model.fit(X_train_filled, y_train)
-    xgb_pred_proba = xgb_model.predict_proba(X_test_filled)
+    xgb_model.fit(X_train_imputed, y_train)
+    xgb_pred_proba = xgb_model.predict_proba(X_test_imputed)
 
     rf_model = RandomForestClassifier()
-    rf_model.fit(X_train_filled, y_train)
-    rf_pred_proba = rf_model.predict_proba(X_test_filled)
+    rf_model.fit(X_train_imputed, y_train)
+    rf_pred_proba = rf_model.predict_proba(X_test_imputed)
 
     etc_model = ExtraTreesClassifier()
-    etc_model.fit(X_train_filled, y_train)
-    etc_pred_proba = etc_model.predict_proba(X_test_filled)
+    etc_model.fit(X_train_imputed, y_train)
+    etc_pred_proba = etc_model.predict_proba(X_test_imputed)
 
     # 각 모델의 예측 확률을 결합하여 최종 예측을 생성한다
     ensemble_pred_proba = (xgb_pred_proba + rf_pred_proba + etc_pred_proba) / 3
@@ -147,11 +126,11 @@ print("=== result : {:.4f} ± {:.4f}".format(sum(accuracy_list)/len(accuracy_lis
 print("=== RMSE result : {:.4f} ± {:.4f}".format(np.mean(rmse_list), np.std(rmse_list)))
 print("==========================================")
 
-# 결과를 DataFrame으로 변환하여 CSV 파일에 추가로 저장
-results_df = pd.DataFrame(results)
-if os.path.exists(result_csv_path):
-    results_df.to_csv(result_csv_path, mode='a', header=False, index=False)
-else:
-    results_df.to_csv(result_csv_path, index=False)
+# # 결과를 DataFrame으로 변환하여 CSV 파일에 추가로 저장
+# results_df = pd.DataFrame(results)
+# if os.path.exists(result_csv_path):
+#     results_df.to_csv(result_csv_path, mode='a', header=False, index=False)
+# else:
+#     results_df.to_csv(result_csv_path, index=False)
 
-print("Results saved to:", result_csv_path)
+# print("Results saved to:", result_csv_path)
