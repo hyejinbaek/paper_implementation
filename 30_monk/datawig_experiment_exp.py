34302692d37d44c236778ff1ddfb693585f9a18c
hyejinbaek <bhj4208@gmail.com>
2024-04-11 23:19:02 +0900

    complete commit

---
+++ 30_monk/datawig_experiment_exp.py
@@ -0,0 +1,205 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from setproctitle import setproctitle
+from datawig import SimpleImputer
+import os
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.preprocessing import MinMaxScaler
+
+# CUDA 환경 설정
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+# 프로세스 제목 설정
+setproctitle('hyejin')
+
+# CSV 파일 경로 설정
+result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/res/0_final/30_monk_ensemble_method_res_final.csv'
+
+# 결과를 저장할 리스트 초기화
+results = []
+rmse_list = []
+
+class DynamicImputationModel:
+    def __init__(self, num_layers, num_hidden, dim_y, num_features):
+        self.num_layers = num_layers
+        self.num_hidden = num_hidden
+        self.dim_y = dim_y
+        self.num_features = num_features
+        tf.compat.v1.disable_eager_execution()
+        self.x = tf.compat.v1.placeholder(tf.float32, shape=[None, self.num_features])
+
+        self.y_true = tf.compat.v1.placeholder(tf.float32, shape=[None, dim_y])
+        self.logits, self.pred = self.build_model(self.x)
+        self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y_true, logits=self.logits))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        self.train_op = self.optimizer.minimize(self.loss)
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+    def build_model(self, x):
+        for _ in range(self.num_layers):
+            x = tf.layers.dense(x, self.num_hidden, activation=tf.nn.tanh)
+        logits = tf.layers.dense(x, self.dim_y)
+
+        if self.dim_y == 1:
+            pred = tf.nn.sigmoid(logits)
+        elif self.dim_y > 2:
+            pred = tf.nn.softmax(logits)
+
+        return logits, pred
+
+    def train_model(self, train_X, train_y, num_epochs, batch_size):
+        num_batches = int(np.ceil(len(train_X) / batch_size))
+        for epoch in range(num_epochs):
+            indices = np.arange(len(train_X))
+            np.random.shuffle(indices)
+            train_X_shuffled = train_X.iloc[indices]
+            train_y_shuffled = train_y.iloc[indices]
+
+            for i in range(num_batches):
+                batch_X = train_X_shuffled.iloc[i * batch_size: (i + 1) * batch_size]
+                batch_y = train_y_shuffled.iloc[i * batch_size: (i + 1) * batch_size]
+
+                self.sess.run(self.train_op, feed_dict={self.x: batch_X.values, self.y_true: batch_y.values.reshape(-1, 1)})
+                # self.sess.run(self.train_op, feed_dict={self.x: batch_X.values, self.y_true: batch_y.values.reshape(-1, self.dim_y)})
+
+
+    def get_accuracy(self, x_tst, y_tst):
+        if self.dim_y == 1:
+            pred_Y = tf.cast(self.pred > 0.5, tf.float32)
+            correct_prediction = tf.equal(pred_Y, self.y_true)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+            acc = self.sess.run(accuracy, feed_dict={self.x: x_tst, self.y_true: y_tst})
+            # acc = self.sess.run(accuracy, feed_dict={self.x: x_tst, self.y_true: y_tst.reshape(-1, self.dim_y)})
+
+
+        else:
+            y_tst_hat = self.sess.run(self.pred, feed_dict={self.x: x_tst})
+            y_tst_hat = np.argmax(y_tst_hat, axis=1)
+
+            acc = accuracy_score(np.argmax(y_tst, axis=1), y_tst_hat)
+
+        return acc
+
+prepro_data = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/preprocessing/30_monk.csv'
+prepro_data = pd.read_csv(prepro_data)
+
+data_pth = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/missing/30_monk.csv'
+df_data = pd.read_csv(data_pth)
+train_col = ['a1', 'a2', 'a3','a4','a5', 'a6']
+
+data_with_missing = df_data
+
+# 반복 횟수 설정
+num_iterations = 30
+
+accuracy_list = []
+imputers = {}
+
+for iteration in range(num_iterations):
+    # Train set과 test set으로 분할
+    train_data, test_data = train_test_split(data_with_missing, test_size=0.2, random_state=iteration)
+    
+    # 데이터 결측치 채우기
+    for col in train_col:
+        imputer = SimpleImputer(
+            input_columns=train_col,
+            output_column=col,
+            output_path=f'./imputer_model/imputer_model_{col}'
+        )
+        imputer.fit(train_df=train_data, num_epochs=5)
+        imputers[col] = imputer
+
+    # Impute missing values for each column in train_data
+    train_imputed_data = {}
+    
+    for col, imputer in imputers.items():
+        predictions = imputer.predict(train_data)
+        train_imputed_data[col] = predictions[col + '_imputed']  # '_imputed' is added by datawig
+
+    # Create a DataFrame with imputed values for train set
+    train_imputed_df = pd.DataFrame(train_imputed_data)
+
+    # Impute missing values for each column in test_data
+    test_imputed_data = {}
+    for col, imputer in imputers.items():
+        predictions = imputer.predict(test_data)
+        test_imputed_data[col] = predictions[col + '_imputed']  # '_imputed' is added by datawig
+
+    # Create a DataFrame with imputed values for test set
+    test_imputed_df = pd.DataFrame(test_imputed_data)
+
+    # 학습을 위한 데이터 준비
+    train_X = train_imputed_df[train_col]
+    train_y = train_data['class']
+    test_X = test_imputed_df[train_col]
+    test_y = test_data['class']
+
+    # 신경망 모델 학습
+    model = DynamicImputationModel(num_layers=3, num_hidden=128, dim_y=1, num_features=len(train_col))  # Pass num_features
+    num_epochs = 50
+    batch_size = 32
+    model.train_model(train_X, train_y, num_epochs, batch_size)
+
+    accuracy = model.get_accuracy(test_X.values, test_y.values.reshape(-1, 1))
+
+    print("==========================================")
+    print(str(iteration+1)+"th accuracy === : ", accuracy)
+    print("==========================================")
+    accuracy_list.append(accuracy)
+
+    model.sess.close()
+
+    # 모든 반복이 끝난 후에 평균 및 표준편차 계산
+    accuracy_mean = np.mean(accuracy_list)
+    accuracy_std = np.std(accuracy_list)
+
+    # 결측치 생성 전의 데이터를 동일하게 train/test로 나누어서 저장
+    original_data_train, original_data_test = train_test_split(prepro_data, test_size=0.2, random_state=iteration)
+    original_data_test = original_data_test.drop(columns=['class'])
+
+    # Min-Max Scaling 수행
+    scaler = MinMaxScaler(feature_range=(-1, 1))  # imputed_test_data와 동일한 범위로 조정
+    original_x_test_scaled = scaler.fit_transform(original_data_test)
+    test_X_scaled = scaler.fit_transform(test_imputed_df)
+    print(" == original_x_test_scaled == ", original_x_test_scaled)
+    print(" == test_X_scaled == ", test_X_scaled)
+
+    # RMSE 계산
+    rmse = sqrt(mean_squared_error(original_x_test_scaled, test_X_scaled))
+    print("==========================================")
+    print(str(iteration + 1) + "th Ensemble Imputation rmse: ", rmse)
+    print("==========================================")
+    rmse_list.append(rmse)
+
+    # 결과를 딕셔너리로 저장
+    result = {
+        'Dataset' : '30_monk',
+        'method' : 'datawig',
+        'Experiment': iteration + 1,
+        'RMSE': "{:.4f} ± {:.4f}".format(np.mean(rmse_list), np.std(rmse_list))
+    }
+    results.append(result)
+
+print("Mean Accuracy: {:.2f}".format(accuracy_mean))
+print("Standard Deviation of Accuracy: {:.2f}".format(accuracy_std))
+print("==========================================")
+print("=== Accuracy result : {:.4f} ± {:.4f}".format(sum(accuracy_list)/len(accuracy_list), np.std(accuracy_list)))
+print("=== RMSE result : {:.4f} ± {:.4f}".format(np.mean(rmse_list), np.std(rmse_list)))
+print("==========================================")
+
+# 결과를 DataFrame으로 변환하여 CSV 파일에 추가로 저장
+results_df = pd.DataFrame(results)
+if os.path.exists(result_csv_path):
+    results_df.to_csv(result_csv_path, mode='a', header=False, index=False)
+else:
+    results_df.to_csv(result_csv_path, index=False)
+
+print("Results saved to:", result_csv_path)
--- dataset_missing_create.py
+++ dataset_missing_create.py
@@ -26,9 +26,8 @@ def build_embedding_model(input_dims, embedding_dims):
         embeddings.append(embedding)
     return inputs, embeddings
 
-
-result_preprocessing_path = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/preprocessing/30_monk.csv'
-result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/missing/30_monk.csv'
+result_preprocessing_path = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/preprocessing/33_forty.csv'
+result_csv_path = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/missing/33_forty.csv'
 
 # 데이터 파일 경로 설정
 data_pth = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/original/33_forty.csv'
@@ -36,12 +35,13 @@ data_pth = '/userHome/userhome2/hyejin/paper_implementation/00_dataset/original/
 # 데이터 불러오기
 df_data = pd.read_csv(data_pth)
 
-df_data.columns = ['class', 'a1', 'a2', 'a3','a4','a5', 'a6', 'id']
-df_data = df_data.drop(labels='id', axis=1)
+# df_data.columns = ['class', 'Repetition', 'PH', 'IFP', 'NLP', 'NGP', 'NGL', 'NS', 'MHG', 'GY']
+# df_data = df_data.drop(labels='id', axis=1)
 
-# df_data = df_data.replace('?', 0).astype(str)
-train_col = ['a1', 'a2', 'a3','a4','a5', 'a6']
-print(df_data)
+# df_data = df_data.replace(1, 0).astype(str)
+# train_col = ['Repetition', 'PH', 'IFP', 'NLP', 'NGP', 'NGL', 'NS', 'MHG', 'GY']
+
+
 # unique_values_A2 = df_data['liver_big'].unique()
 # print("Unique values in column A2:", unique_values_A2)
 # train_col = ['age', 'oper', 'positive']
@@ -52,7 +52,8 @@ print(df_data)
 # train_col.remove('class')
 
 data = df_data
-# print(data)
+
+
 # categorical_columns = ['class']
 
 # # 레이블 인코딩 적용
@@ -62,18 +63,18 @@ data = df_data
 # # df_data = pd.get_dummies(df_data, columns=['class'], prefix='class')
 # print(data)
 
-# 데이터전처리 후 데이터프레임을 CSV 파일로 저장
-data.to_csv(result_preprocessing_path, index=False)
-print("Data preprocessing saved to CSV file.")
+# # 데이터전처리 후 데이터프레임을 CSV 파일로 저장
+# data.to_csv(result_preprocessing_path, index=False)
+# print("Data preprocessing saved to CSV file.")
 
-missing_length = 0.2
-for col in train_col:
-    nan_mask = np.random.rand(data.shape[0]) < missing_length
-    data.loc[nan_mask, col] = np.nan
+# missing_length = 0.2
+# for col in train_col:
+#     nan_mask = np.random.rand(data.shape[0]) < missing_length
+#     data.loc[nan_mask, col] = np.nan
 
-data_with_missing = data
-print(data_with_missing)
+# data_with_missing = data
+# print(data_with_missing)
 
-# 결측치 생성 후 데이터프레임을 CSV 파일로 저장
-data_with_missing.to_csv(result_csv_path, index=False)
-print("Data with missing values saved to CSV file.")
+# # 결측치 생성 후 데이터프레임을 CSV 파일로 저장
+# data_with_missing.to_csv(result_csv_path, index=False)
+# print("Data with missing values saved to CSV file.")
